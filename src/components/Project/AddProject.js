c540d003479d31808456ff6095a45468a1b70b1c
Lidia Belyaeva <lvcaciag@gmail.com>
2021-09-13 17:52:42 +0300

    small updates, connected to back-end

--- src/components/Project/AddProject.js
+++ src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
  class AddProject extends Component {
      constructor(){
@@ -27,7 +30,7 @@ import React, { Component } from 'react'
             "start_date": this.state.start_date,
             "end_date":this.state.end_date    
          };
-         console.log(newProject);
+         this.props.createProject(newProject, this.props.history);
      }
 
     render() {
@@ -89,4 +92,11 @@ import React, { Component } from 'react'
         )
     }
 }
-export default AddProject;
+AddProject.propTypes = {
+    createProject: PropTypes.func.isRequired
+  };
+  
+  export default connect(
+    null,
+    { createProject }
+  )(AddProject);
